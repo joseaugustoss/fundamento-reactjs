699124edc23e5b02590f10e370ab79b6f02be210
José Augusto <joseaugusto0408@gmail.com>
2022-06-20 11:47:25 -0300

    componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
+import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <h2>Area dos posts</h2>
+        </main>
+      </div>
+    </>
+  );
 }
