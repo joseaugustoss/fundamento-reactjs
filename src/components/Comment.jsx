aa017775ba4c14a5f6c0f24ddfe0d6b788f92b30
José Augusto <joseaugusto0408@gmail.com>
2022-06-30 13:53:06 -0300

    componente avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,15 @@
 import { Trash, ThumbsUp } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/joseaugustoss.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/joseaugustoss.png"
+        alt=""
+      />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
